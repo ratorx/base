0f3a5b3ce66cb07ad8f6b3b07ff693cee6b50464
Reeto Chatterjee <reeto@google.com>
2022-12-22 00:11:23 +0000

    Improve shell.nix

--- default/shell.nix
+++ default/shell.nix
@@ -1,11 +1,11 @@
 (import
   (
     let
-      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+      flake-compat = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.flake-compat.locked;
     in
     fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-      sha256 = lock.nodes.flake-compat.locked.narHash;
+      url = "https://github.com/${flake-compat.owner}/${flake-compat.repo}/archive/${flake-compat.rev}.tar.gz";
+      sha256 = flake-compat.narHash;
     }
   )
   { src = ./.; }).defaultNix.devShells.${builtins.currentSystem}.default
