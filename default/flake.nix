19371288248e99160a158be36a3faca00359d9a8
Reeto Chatterjee <me@ree.to>
2023-03-03 16:05:32 +0000

    Use nixpkgs-unstable
    It doesn't have CI, but is more likely to have binary packages for
    aarch64-darwin.

--- default/flake.nix
+++ default/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-major.minor";
   };
   outputs = { self, flake-utils, nixpkgs, ... }:
